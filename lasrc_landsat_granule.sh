7a8e75088a37326af5177177876c02af545fa8d9
Sean Harkins <sharkinsgis@gmail.com>
2019-09-10 14:27:17 -0400

    Finalize ESPA conversion script.

--- lasrc_landsat_granule.sh
+++ lasrc_landsat_granule.sh
@@ -2,16 +2,21 @@
 id="LC08_L1TP_027039_20190901_20190901_01_RT"
 
 IFS='_'
-read -ra ADDR <<< "$id" # str is read into an array as tokens separated by IFS
+Read into an array as tokens separated by IFS
+read -ra ADDR <<< "$id"
+# Format GCS url and download
 url=gs://gcp-public-data-landsat/LC08/01/${ADDR[2]:0:3}/${ADDR[2]:3:5}/${id}/
 gsutil -m cp -r "$url" /tmp
 landsatdir=/tmp/${id}
 cd "$landsatdir"
-ls
+Convert data from tiled to scanline for espa formatting
 for f in *.TIF
-	do gdal_translate -co TILED=NO "%f" "%f"
+	do
+	gdal_translate -co TILED=NO "$f" "${f}_scan"
+	rm "$f"
+	mv "${f}_scan" "$f"
 	done
 mtl=${id}_MTL.txt
+# Convert to espa format
 convert_lpgs_to_espa --mtl "$mtl"
-ls
 
