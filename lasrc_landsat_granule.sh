54da02a312c3236a98af1c76e708d999a64ea0f4
Sean Harkins <sharkinsgis@gmail.com>
2019-09-10 17:22:16 -0400

    Finalize LaSRC processing approach for ECS.

--- lasrc_landsat_granule.sh
+++ lasrc_landsat_granule.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-id="LC08_L1TP_027039_20190901_20190901_01_RT"
-
+# id="LC08_L1TP_027039_20190901_20190901_01_RT"
+id=$LANDSAT_SCENE_ID
 IFS='_'
-Read into an array as tokens separated by IFS
+# Read into an array as tokens separated by IFS
 read -ra ADDR <<< "$id"
 # Format GCS url and download
 url=gs://gcp-public-data-landsat/LC08/01/${ADDR[2]:0:3}/${ADDR[2]:3:5}/${id}/
 gsutil -m cp -r "$url" /tmp
 landsatdir=/tmp/${id}
 cd "$landsatdir"
-Convert data from tiled to scanline for espa formatting
+# Convert data from tiled to scanline for espa formatting
 for f in *.TIF
 	do
 	gdal_translate -co TILED=NO "$f" "${f}_scan"
@@ -19,4 +19,6 @@ for f in *.TIF
 mtl=${id}_MTL.txt
 # Convert to espa format
 convert_lpgs_to_espa --mtl "$mtl"
-
+# Run lasrc
+do_lasrc.py --xml "${id}.xml"
+ls
